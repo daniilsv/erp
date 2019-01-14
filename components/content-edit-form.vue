10efe970b35644087d7f671de487c70770e101c9
DVS <danstrrr@yandex.ru>
2019-01-15 00:31:58 +0300

    add editable pre form gen

---
+++ components/content-edit-form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="(field, index) in fields" :key="index">
+      <CEFField v-model="fields[index]"/>
+    <v-btn @click="change(index)"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CEFField from "~/components/content-edit-form-field.vue";
+export default {
+  props: ["fields"],
+  components: { CEFField },
+  methods: {
+    change(index) {
+      this.fields[index].type="checkbox";
+    }
+  }
+};
+</script>
---
+++ pages/project/form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <CEForm :fields="data"/>
+    <v-btn @click="add"/>
+  </div>
+</template>
+
+<script>
+import CEForm from "~/components/content-edit-form.vue";
+export default {
+  components: { CEForm },
+  data() {
+    return {
+      data: [
+        {
+          type: "text",
+          label: "label",
+          value: "value"
+        },
+        {
+          type: "textarea",
+          label: "Use password?",
+          value: "usePassword"
+        }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      this.data.push({
+        type: "textarea",
+        label: "Use password?",
+        value: "usePassword"
+      });
+    }
+  }
+};
+</script>
